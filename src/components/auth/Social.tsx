a5445469b8b83b5f55475cd07d99f0dc8768fcce
Hisham <hishamarshad46@gmail.com>
2025-07-18 19:56:22 +0500

    Commit

--- src/components/auth/Social.tsx
+++ src/components/auth/Social.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 export default function Social() {
   const googleClientId = "11171866817-27irv6kp4bf54pdogq5mh497ao65psn6.apps.googleusercontent.com"; // Replace with your actual Google Client ID
-  const redirectUri = "http://localhost:3000"; // Your frontend callback URI
+  const redirectUri = "https://chatapp-coral-one.vercel.app"; // Your frontend callback URI
 
   const handleLogin = () => {
     const googleAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${googleClientId}&redirect_uri=${redirectUri}&scope=openid%20email%20profile`;
